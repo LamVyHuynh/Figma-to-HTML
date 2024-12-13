9c47490c793ad242b61819ce9f4258392237fda5
LamVyHuynh <huynhlamvy145@gmail.com>
2024-12-13 11:13:34 +0700

    them phan ban chay voi them js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function ShowList() {
+  // tự động đóng mở
+  document.getElementById("danhSach").classList.toggle("unShow");
+}
+
+function ShowProduct() {
+  // tự động đóng mở
+  document.getElementById("danhSachSP").classList.toggle("unShow");
+}
