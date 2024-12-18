404fa5948823d6ad15cde693d26bd88c30c6e353
LamVyHuynh <huynhlamvy145@gmail.com>
2024-12-18 10:41:38 +0700

    da lam xong list pd vs 1 phan cua hang

--- js/index.js
+++ js/index.js
@@ -6,4 +6,25 @@ function ShowList() {
 function ShowProduct() {
   // tự động đóng mở
   document.getElementById("danhSachSP").classList.toggle("unShow");
+  document.getElementById("seeMore").classList.toggle("unShow");
 }
+
+const ListProduct = document.querySelector(".list-product");
+const SeeMore = document.querySelector(".see_more");
+const iconShowDown = document.querySelector(".icon-see_down");
+const iconSHowUp = document.querySelector(".icon-see_up");
+let isSeeMore = false;
+SeeMore.addEventListener("click", () => {
+  isSeeMore = !isSeeMore;
+  if (isSeeMore) {
+    ListProduct.classList.add("showListPr");
+    ListProduct.classList.add("hieuUngShow");
+    iconShowDown.classList.add("unShow");
+    iconSHowUp.classList.remove("unShow");
+  } else {
+    ListProduct.classList.remove("showListPr");
+    ListProduct.classList.add("hieuUngShow");
+    iconShowDown.classList.remove("unShow");
+    iconSHowUp.classList.add("unShow");
+  }
+});
